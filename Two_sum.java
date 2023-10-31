16f37ec87e0c2f2f786dc005d1b7b9bbfdea228d
Prafull <official.sonawaneprafull@gmail.com>
2023-11-01 01:24:27 +0530

    leet code problem 1, better solution added

--- Two_sum.java
+++ Two_sum.java
@@ -1,34 +1,55 @@
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Two_sum {
 
-    public static void better_twoSum(int nums[], int target) {
-        
+    public static int[] better(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+        // Add element into hashmap
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            int substraction = target - nums[i];
+            if(map.containsKey(substraction) && map.get(substraction) != i) {
+                System.out.println(i + " " + map.get(substraction));
+                return new int[]{i,map.get(substraction)};
+            }
+
+        }
+        return new int[]{-1,-1};
     }
 
     public static int[] bruteForce_twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length - 1; i++) {
-            for(int j = i+1; j < nums.length; j++) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
 
-                if(nums[i] + nums[j] == target) {
+                if (nums[i] + nums[j] == target) {
                     // System.out.println(i + " " + j);
-                    int ans[] = {i,j};
+                    int ans[] = { i, j };
                     return ans;
                 }
             }
         }
 
-        int non[] = {-1,-1};
+        int non[] = { -1, -1 };
         return non;
     }
+
     public static void main(String[] args) {
-        int[] arr = {3,3};
-        int target = 6;
+        int[] arr = { 4,8,0};
+        int target = 4;
 
-        int[] ans = bruteForce_twoSum(arr, target);
+        // int[] ans = bruteForce_twoSum(arr, target);
 
-        // System.out.println(ans);
-        for(int i : ans){
-            System.out.println(i);
-        }
+        // // System.out.println(ans);
+        // for(int i : ans){
+        // System.out.println(i);
+        // }
+
+        better(arr, target);
     }
 }
