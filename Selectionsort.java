1529302a4186632fd212ec2a5aeb50defe07a65e
Prafull <official.sonawaneprafull@gmail.com>
2023-11-21 16:15:34 +0530

    Improved Selection sort by removing unneccasory variables

--- Selectionsort.java
+++ Selectionsort.java
@@ -5,15 +5,12 @@ public class Selectionsort {
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
-            int minIdx = i;
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIdx]) {
-                    minIdx = j;
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
-                // swap
-                int temp = arr[i];
-                arr[i] = arr[minIdx];
-                arr[minIdx] = temp;
             }
         }
 
